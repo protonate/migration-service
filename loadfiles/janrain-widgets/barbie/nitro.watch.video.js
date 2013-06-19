3a2c7f9c0b9f2e2257d97602579e9744d2c41c38
nathan-g <nathan.g@janrain.com>
2013-06-18 23:38:29 -0700

    remove files

--- loadfiles/janrain-widgets/barbie/nitro.watch.video.js
+++
@@ -1,7 +0,0 @@
-﻿jQuery(window).load(function () {
-    var episodeNumber = document.URL.split('/')[5];
-    var watchAction = 'WATCH_EPISODE' + episodeNumber + '_BLID';
-    var rankAction = 'RANK_EPISODE' + episodeNumber + '_BLID';
-    jQuery('.selectME li').attr('data-actions', rankAction);
-    jQuery('#fs').attr('data-actions', watchAction);
-});
